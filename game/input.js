f0e79104362fc853f72f6be43c0786815f846cc8
Chris Weaver <chris.shylor@gmail.com>
2015-06-18 15:37:11 -0400

    added keyboard tracking and cleaned up mouse tracking

--- game/input.js
+++ game/input.js
@@ -3,6 +3,7 @@ var input = function() {
 	this.y = 0;
 	this.clicked = 0;
 	this.clicks = 0;
+	this.keys = new Array();
 }
 
 // return if a click has been detected or not
@@ -15,24 +16,48 @@ input.prototype.click = function() {
 	return false;
 }
 
-// update the mouse xy locations, this should not be called externally
-input.prototype.mouse_xy = function(evt) {
-	var game_window = canvas.getBoundingClientRect();
-	return {
-		x: Math.floor(evt.clientX - game_window.left),
-		y: Math.floor(evt.clientY - game_window.top)
+input.prototype.keydown = function(e) {
+	var found = 0;
+	for(var i=0; i<=this.keys.length; i++) {
+		if(this.keys[i] == e.keyCode) {
+			found = 1;
+		}
+	}
+	if(found == 0) {
+		this.keys.push(e.keyCode);
+		console.log(this.keys);
+	}
+}
+input.prototype.keyup = function(e) {
+	for(var i=0; i<=this.keys.length; i++) {
+		if(this.keys[i] = e.keyCode) {
+			this.keys.splice(i, 1);
+		}
 	}
 }
 
+// update the mouse xy locations, this should not be called externally
+input.prototype.mouse_xy = function(e) {
+	var screen = canvas.getBoundingClientRect();
+	this.x = Math.floor(e.clientX - screen.left);
+	this.y = Math.floor(e.clientY - screen.top);
+}
+
 // detect if the mouse has moved if so update the mouse xy location
-canvas.addEventListener('mousemove', function(evt) {
-	var mouse_xy = input.mouse_xy(evt);
-	input.x = mouse_xy.x;
-	input.y = mouse_xy.y;
+canvas.addEventListener('mousemove', function(e) {
+	input.mouse_xy(e);
 });
 
 // check to see if the user clicked the mouse
 canvas.addEventListener('click', function(evt) {
 	input.clicked = 1;
 	input.clicks = input.clicks + 1;
+});
+
+window.addEventListener('keydown', function(e) {
+	input.keydown(e);
+});
+
+window.addEventListener('keyup', function(e) {
+	input.keyup(e);
 });
